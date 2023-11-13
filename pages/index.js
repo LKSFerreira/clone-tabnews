f6fca5c9e55a55cedd6b7f8402f0ffe24a443ab3
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-11-13 18:59:13 -0300

    :sparkles: Criado o index.js
    :one: Além do arquivo index.js criado, também existe o arquivo index.css no qual criei , sem sucesso para estilizar.

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function Home() {
+    return <div class="content">
+    <h1 class="title">"Não espere resultados diferente fazendo exatamente as mesmas coisas."
+      <div class="aurora">
+        <div class="aurora__item"></div>
+        <div class="aurora__item"></div>
+        <div class="aurora__item"></div>
+        <div class="aurora__item"></div>
+      </div>
+    </h1>
+    <p class="subtitle">Albert Einstein</p>
+  </div>
+}
+
+export default Home;
