bac25c2dc7f98c64b894f3512b03dbd1f20af83a
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-11-16 13:44:42 -0300

    :sparkles: Atualizada frase do dia.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return <div className="content">
-    <h1 className="title">"Não espere resultados diferentes fazendo exatamente as mesmas coisas."
+    <h1 className="title">"Quando o último peixe for pescado, a última árvore derrubada e o último rio poluído, o ser humano vai perceber que dinheiro não se come ."
       <div className="aurora">
         <div className="aurora__item"></div>
         <div className="aurora__item"></div>
@@ -8,7 +8,7 @@ function Home() {
         <div className="aurora__item"></div>
       </div>
     </h1>
-    <p className="subtitle">Albert Einstein</p>
+    <p className="subtitle">Provérbio Indígena</p>
   </div>
 }
 
