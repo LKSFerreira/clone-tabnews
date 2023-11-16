5fa7539fbecaed23ab309c1e3cbfe04b8a204873
Lucas Ferreira <103146024+LKSFerreira@users.noreply.github.com>
2023-11-16 14:38:34 -0300

    :bug: Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return <div className="content">
-    <h1 className="title">"Quando o último peixe for pescado, a última árvore derrubada e o último rio poluído, o ser humano vai perceber que dinheiro não se come ."
+    <h1 className="title">"Quando o ultimo peixe for pescado, a ultima árvore derrubada e o ultimo rio poluído, o ser humano vai perceber que dinheiro não se come ."
       <div className="aurora">
         <div className="aurora__item"></div>
         <div className="aurora__item"></div>
