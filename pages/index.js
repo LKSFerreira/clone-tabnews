ecd9a72eb2ef63f42617dda21717073cf61778dc
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-11-17 14:24:37 -0300

    :sparkles: Copiado código codepen
    :one: O desafio era fazer o projeto rodar um arquivo js que manipulava o DOM completamente

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-function Home() {
-  return <div className="content">
-    <h1 className="title">"Vanderléia, vai trabalhar."
-      <div className="aurora">
-        <div className="aurora__item"></div>
-        <div className="aurora__item"></div>
-        <div className="aurora__item"></div>
-        <div className="aurora__item"></div>
-      </div>
-    </h1>
-    <p className="subtitle">Lucas Ferreira</p>
-  </div>
-}
+import Script from 'next/script'
 
-export default Home;
+export default function Home() {
+  return (
+    <div>
+      <Script src="/brain.js" strategy="beforeInteractive" />
+    </div>
+  )
+}
