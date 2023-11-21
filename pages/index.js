bb22111146051e602aa79bb49f03ef9fd02b9dd0
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-11-20 21:00:13 -0300

    :x: Realizando teste no arquivo `index.js`
    :one: Adicionado funções de testes que serão `removidas` futuramente.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Script from 'next/script'
 
-export default function Home() {
+function Home() {
   return (
     <div>
       <Script src="/brain.js" strategy="beforeInteractive" />
     </div>
   )
 }
+
+export default Home
+
+function Test() {
+    return (
+    <div>
+      <Script src="/brain.js" strategy="beforeInteractive" />
+    </div>
+  )
+}
+
+function Test2()
+{    console.log("test2");};
