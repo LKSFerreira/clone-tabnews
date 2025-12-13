e03093501813aa2155eb7e799f10050f6c84cfc1
Lucas Ferreira <lks_ferreira@hotmail.com>
2025-12-13 01:16:07 -0300

    :sparkles: Melhorado endpoint status com query parametrizada e testes de versão

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as Soma;");
-  console.log(result.rows);
-  response.status(200).json({ status: "OK" });
+
+  const versionResult = await database.query("SHOW server_version;");
+  const version = versionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  // Sanatização feito pelo node-postgres
+  const openedConnectionsResult = await database.query({
+    values: [databaseName],
+    text: "SELECT * FROM pg_stat_activity WHERE datname = $1;",
+  });
+  const openedConnections = openedConnectionsResult.rowCount;
+
+  const status_api = {
+    updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConnections),
+        opened_connections: parseInt(openedConnections),
+        version: version
+      }
+    }
+  };
+
+  response.status(200).json(status_api);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,48 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET to /api/v1/status", () => {
+  let response;
+  let originalBody;
+  let responseBody;
+
+  beforeAll(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
+    originalBody = await response.json();
+  });
+
+  beforeEach(() => {
+    responseBody = JSON.parse(JSON.stringify(originalBody));
+  });
+
+  test("Deveria retornar 200", async () => {
+    expect(response.status).toBe(200);
+  });
+
+  test("Deveria retornar uma data válida", async () => {
+    const parsedUpdatedAt = new Date( responseBody.updated_at).toISOString();
+
+    expect(responseBody.updated_at).toBeDefined();
+    expect(responseBody.updated_at).toEqual(expect.any(String));
+    expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+    expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+  });
+
+  test("Deveria retornar o máximo de conexões no banco de dados", () => {
+    expect(responseBody.dependencies.database.max_connections).toBe(100);
+  });
+
+  test("Deveria retornar o número de conexões abertas no banco de dados", () => {
+    console.log(responseBody.dependencies.database.opened_connections);
+    expect(responseBody.dependencies.database.opened_connections).toBe(1);
+  });
+
+  test("Deveria retornar o número da versão do banco postgres", () => {
+    const version = responseBody.dependencies.database.version;
+    const parsedVersion = parseInt(version);
+
+    expect(version).toBeDefined();
+    expect(version).toEqual(expect.any(String));
+    expect(parsedVersion).toBeGreaterThan(15);
+    expect(parsedVersion).toBeGreaterThanOrEqual(16);
+    expect(parsedVersion).toBeLessThan(17);
+  });
+
 });
